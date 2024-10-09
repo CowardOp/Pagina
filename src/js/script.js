99e2a745f7f691bd47807f089fe47be91311f42a
Brayan Casallas <beto980730@gmail.com>
2024-10-09 17:02:02 -0500

    new

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,9 @@
 let cambioTema = document.getElementById("cambioTema");
-let usuario = document.getElementById("usuario");
 
 cambioTema.addEventListener("change", () => {
   if (cambioTema.checked) {
-    usuario.classList.remove("light-theme");
-    usuario.classList.add("dark-theme");
+    document.body.classList.add("dark-theme");
   } else {
-    usuario.classList.remove("dark-theme");
-    usuario.classList.add("light-theme");
+    document.body.classList.remove("dark-theme");
   }
 });
